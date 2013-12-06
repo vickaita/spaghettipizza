2f5817c12b874ffeb7a293223d1757ec4305f68d
Vick Aita <vickaita@gmail.com>
2013-12-05 22:43:39 -0500

    Cleanup.

--- project.clj
+++ project.clj
@@ -4,9 +4,13 @@
 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2080"]
-                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]]
+                 [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
+                 [org.clojure/core.typed "0.2.19"]]
 
-  :plugins [[lein-cljsbuild "1.0.0-alpha2"]]
+  :plugins [[lein-cljsbuild "1.0.0-alpha2"]
+            [lein-typed "0.3.1"]
+            [lein-kibit "0.0.8"]
+            [com.cemerick/austin "0.1.3"]]
 
   :source-paths ["src"]
 
@@ -17,4 +21,6 @@
                 :output-to "pizza.js"
                 :output-dir "out"
                 :optimizations :none
-                :source-map true}}]})
+                :source-map true}}]}
+
+  :core.typed  {:check [pizza.core pizza.pizza pizza.spaghetti pizza.svg]})
--- src/pizza/core.cljs
+++ src/pizza/core.cljs
@@ -3,7 +3,7 @@
   (:require [goog.dom :as dom]
             [goog.dom.classlist :as cls]
             [goog.events :as evt]
-            [cljs.core.async :refer  [put! chan <!]]
+            [cljs.core.async :refer [put! chan <! map<]]
             [clojure.browser.repl :as repl]
             [pizza.svg :as svg]
             [pizza.pizza :as pzz]
@@ -22,14 +22,14 @@
 
 (defn enable-spaghetti-drawing
   [svg-elem]
-  (let [down (event-channel "mousedown" svg-elem)
-        move (event-channel "mousemove" svg-elem)
+  (let [down (map< e->pt (event-channel "mousedown" svg-elem))
+        move (map< e->pt (event-channel "mousemove" svg-elem))
         up (event-channel "mouseup" js/document)]
     (go (while true
-          (alt! down ([e] (let [noodle (spg/create-topping @current-tool (e->pt e))]
-                       (reset! current-noodle noodle)
-                       (dom/append svg-elem (:element noodle))))
-                move ([e] (swap! current-noodle spg/add-point! (e->pt e)))
+          (alt! down ([e] (let [n (spg/create-topping @current-tool e)]
+                            (reset! current-noodle n)
+                            (dom/append svg-elem (:element n))))
+                move ([e] (swap! current-noodle spg/add-point! e))
                 up (reset! current-noodle nil))))))
 
 (defn enable-tool-selection
@@ -39,13 +39,13 @@
           (let [elem (.-target (<! clicks))
                 tool (keyword (.getAttribute elem "data-tool"))]
             (when tool
-              (do (reset! current-tool tool)
-                  (let [elems (dom/getChildren toolbar)]
-                    (dotimes [i (alength elems)]
-                      (let [el (aget elems i)]
-                        (if (= elem el)
-                          (cls/add elem "active")
-                          (cls/remove el "active"))))))))))))
+              (reset! current-tool tool)
+              (let [elems (dom/getChildren toolbar)]
+                (dotimes [i (alength elems)]
+                  (let [el (aget elems i)]
+                    (if (= elem el)
+                      (cls/add elem "active")
+                      (cls/remove el "active")))))))))))
 
 (defn main
   []
@@ -57,4 +57,4 @@
     (enable-tool-selection (dom/getElement "toolbar"))))
 
 (evt/listen js/document "DOMContentLoaded" main)
-;(repl/connect "http://ui:9000/repl")
+(repl/connect "http://ui:9000/repl")
