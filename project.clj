70f6e7d0bd099735da5e1ec3f018503d07c1e25e
Vick Aita <vickaita@gmail.com>
2014-01-07 23:08:34 -0500

    Starting to switch away from Amazonica.

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [org.clojure/clojurescript "0.0-2138"]
                  [org.clojure/core.async "0.1.242.0-44b1e3-alpha"]
-                 [org.clojure/core.typed "0.2.19"]
+                 [org.clojure/core.typed "0.2.21"]
                  [org.clojure/tools.logging "0.2.6"]
                  [commons-codec/commons-codec "1.4"]
                  [ring "1.2.1"]
@@ -13,6 +13,8 @@
                  [compojure "1.1.6"]
                  [hiccup "1.0.4"]
                  [amazonica "0.1.32"]
+                 [clj-aws-s3 "0.3.7"]
+                 [com.cemerick/rummage "1.0.1"]
                  [digest "1.4.3"]
                  [prismatic/dommy "0.1.1"]
                  [environ "0.4.0"]]
@@ -41,10 +43,10 @@
                            :output-dir "resources/public/js"
                            :optimizations :advanced}}]}
   ;:core.typed {:check [pizza.types]}
-  :profiles {:dev {:env {:mode :dev}}
-             :test {:env {:mode :test}
+  :profiles {:dev {:env {:environment :dev}}
+             :test {:env {:environment :test}
                     :aot [pizza.handler]}
-             :prod {:env {:mode :prod}
+             :prod {:env {:environment :prod}
                     :aot [pizza.handler]}}
   :aliases {"push-ui" ["run" "-m" "pizza.tasks/push-ui"]
             "push-api" ["run" "-m" "pizza.tasks/push-api"]})
