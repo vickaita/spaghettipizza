feaa9e752e2138d4dc1301c76d92908a574c8527
Vick Aita <vickaita@gmail.com>
2014-02-21 21:06:05 -0500

    v0.2.9-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pizza "0.2.8"
+(defproject pizza "0.2.9-SNAPSHOT"
   :description "Spaghetti Pizza -- the original pasta on pizza simulator!"
   :url "http://spaghettipizza.us"
   :dependencies [[org.clojure/clojure "1.5.1"]
