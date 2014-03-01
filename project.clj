aff17f78608e31371d7b129ce48e4e08480b2071
Vick Aita <vickaita@gmail.com>
2014-03-01 12:18:02 -0500

    Update cljs to 2173

--- project.clj
+++ project.clj
@@ -2,7 +2,7 @@
   :description "Spaghetti Pizza -- the original pasta on pizza simulator!"
   :url "http://spaghettipizza.us"
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [org.clojure/clojurescript "0.0-2156"]
+                 [org.clojure/clojurescript "0.0-2173"]
                  [org.clojure/core.async "0.1.267.0-0d7780-alpha"]
                  [org.clojure/core.typed "0.2.34"]
                  [org.clojure/tools.logging "0.2.6"]
