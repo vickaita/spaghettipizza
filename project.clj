9fe5b0ccd43e9a9985b41d33f82b687eed894f42
Vick Aita <vickaita@gmail.com>
2014-03-02 20:06:58 -0500

    Bump version for development.

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject pizza "0.3.0"
+(defproject pizza "0.4.0-SNAPSHOT"
   :description "Spaghetti Pizza -- the original pasta on pizza simulator!"
   :url "http://spaghettipizza.us"
   :dependencies [[org.clojure/clojure "1.5.1"]
