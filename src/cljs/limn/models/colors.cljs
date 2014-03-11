781372c7bc77542f7a4fe4623a0086870ad47b51
Vick Aita <vickaita@gmail.com>
2014-03-10 20:48:56 -0400

    Reorder the colors so that yellow is the default.

--- src/cljs/limn/models/colors.cljs
+++ src/cljs/limn/models/colors.cljs
@@ -2,12 +2,12 @@
 
 (def ^:private palettes
   [{:name "Default Colors"
-    :colors [{:name "Red" :fill "#F86969" :stroke "#F04F4F"}
+    :colors [{:name "Yellow" :fill "#FAE265" :stroke "#DDAB0B"}
              {:name "Orange" :fill "#EF951B" :stroke "#B55F11"}
-             {:name "Yellow" :fill "#FAE265" :stroke "#DDAB0B"}
-             {:name "Green" :fill "#83D874" :stroke "#5FBA52"}
-             {:name "Blue" :fill "#6EB4E1" :stroke "#1E6B92"}
+             {:name "Red" :fill "#F86969" :stroke "#F04F4F"}
              {:name "Purple" :fill "#D459A4" :stroke "#AE4173"}
+             {:name "Blue" :fill "#6EB4E1" :stroke "#1E6B92"}
+             {:name "Green" :fill "#83D874" :stroke "#5FBA52"}
              {:name "White" :fill "#F2F2F2" :stroke "#D9D9D9"}
              {:name "Black" :fill "#202020" :stroke "#181818"}]}])
 
