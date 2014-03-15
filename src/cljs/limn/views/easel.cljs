60bcc82f72879d33bd083cb86e54c024833f1020
Vick Aita <vickaita@gmail.com>
2014-03-15 14:03:02 -0400

    Move event handler definitions out of component.

--- src/cljs/limn/views/easel.cljs
+++ src/cljs/limn/views/easel.cljs
@@ -15,23 +15,16 @@
   (let [view-offset-x (:view-offset-x app)
         view-offset-y (:view-offset-y app)
         scale (:scale-by app)]
-    [(-> x (* scale) (+ view-offset-x) Math/floor)
-     (-> y (* scale) (+ view-offset-y) Math/floor)]))
+    [(-> x (+ view-offset-x) (* scale) Math/floor)
+     (-> y (+ view-offset-y) (* scale) Math/floor)]))
 
-(defn- normalize-points
+(defn- normalized-points
   "Returns a vector of points from an event. Click events will always return a
   vector with a single point in it; touch events will return a point for each
   finger that is touching. Points will be offset so that they are relative to
-  the svg element, but they are still in the user-space."
-  [e]
-  (when-let [elem (.getElementById js/document "align-svg")]
-    (let [; XXX: There is a bug in React.js that incorrectly reports #document as
-          ; the current target. When this is fixed upstream then getElement call
-          ; can be avoided. Fixed with this pull request:
-          ; https://github.com/facebook/react/pull/747, but not in current
-          ; release yet.
-          ;elem (.getElementById js/document "align-svg") ;elem (.-currentTarget e)
-          rect (.getBoundingClientRect elem)
+  element, but they are still in the user-space."
+  [element e]
+    (let [rect (.getBoundingClientRect element)
           left (.-left rect)
           top (.-top rect)]
       (case (.-type e)
@@ -41,60 +34,61 @@
             [(- (.-pageX t) left) (- (.-pageY t) top)]))
         "touchend"
         nil
-        [[(- (.-pageX e) left) (- (.-pageY e) top)]]))))
+        [[(- (.-pageX e) left) (- (.-pageY e) top)]])))
+
+(defn- handle-start
+  [app owner e]
+  (doto e .preventDefault .stopPropagation)
+  (let [points (normalized-points (om/get-node owner) e)]
+    (case (count points)
+      1 (let [state (om/get-state owner)
+              skin (:skin state)
+              color (:color state)]
+          (om/set-state! owner :drawing? true)
+          (om/transact!
+            app #(me/start-stroke % (translate @app (first points)) skin color)))
+      2 (om/set-state! owner :zoom (map (partial translate @app) points)))))
+
+(defn- handle-move
+  [app owner e]
+  (doto e .preventDefault .stopPropagation)
+  (let [points (normalized-points (om/get-node owner) e)
+        state (om/get-state owner)]
+    (case (count points)
+      1 (when (:drawing? state)
+          (om/transact! app #(me/extend-stroke % (translate @app (first points)))))
+      2 (when-let [zoom (:zoom state)]
+          (om/transact! app #(me/zoom % zoom points))))))
+
+(defn- handle-end
+  [app owner e]
+  (doto e .preventDefault .stopPropagation)
+  (let [state (om/get-state owner)]
+    (when (:drawing? state)
+      (om/set-state! owner :drawing? false)
+      (om/transact! app #(me/end-stroke %)))
+    (when (:zoom state)
+      (om/set-state! owner :zoom nil))))
 
 (defn easel
   [app owner]
-  (let [commands (om/get-shared owner :commands)
-
-        start-stroke
-        (fn [e]
-          (doto e .preventDefault .stopPropagation)
-          (let [points (normalize-points e)]
-            (case (count points)
-              1 (let [state (om/get-state owner)
-                      skin (:skin state)
-                      color (:color state)]
-                  (om/set-state! owner :drawing? true)
-                  (om/transact! app #(me/start-stroke % (translate @app (first points)) skin color)))
-              2 (om/set-state! owner :zoom (map translate points)))))
-
-        extend-stroke
-        (fn [e]
-          (doto e .preventDefault .stopPropagation)
-          (let [points (normalize-points e)
-                state (om/get-state owner)]
-            (case (count points)
-              1 (when (:drawing? state)
-                  (om/transact! app #(me/extend-stroke % (translate @app (first points)))))
-              2 (when-let [zoom (:zoom state)]
-                  (om/transact! app #(me/zoom % zoom points))))))
-
-        end-stroke
-        (fn [e]
-          (doto e .preventDefault .stopPropagation)
-          (let [state (om/get-state owner)]
-            (when (:drawing? state)
-              (om/set-state! owner :drawing? false)
-              (om/transact! app #(me/end-stroke %)))
-            (when (:zoom state)
-              (om/set-state! owner :zoom nil))))
-
-        ]
+  (let [start-stroke (partial handle-start app owner)
+        extend-stroke (partial handle-move app owner)
+        end-stroke (partial handle-end app owner)]
     (reify
       om/IWillMount
       (will-mount [_] (events/listen js/document "mouseup" end-stroke))
       om/IRender
       (render [_]
         (let [side (min (:width app) (:height app))]
-          (html [:section.easel {:id "align-svg"
-                                 :width side
-                                 :height side
-                                 :on-mouse-down start-stroke
-                                 :on-touch-start start-stroke
-                                 :on-mouse-move extend-stroke
-                                 :on-touch-move extend-stroke
-                                 :on-touch-end end-stroke}
+          (html [:section.easel.align-svg
+                 {:width side
+                  :height side
+                  :on-mouse-down start-stroke
+                  :on-touch-start start-stroke
+                  :on-mouse-move extend-stroke
+                  :on-touch-move extend-stroke
+                  :on-touch-end end-stroke}
                  [:svg {:id "main-svg"
                         :width side
                         :height side
