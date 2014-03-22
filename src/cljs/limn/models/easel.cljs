45f4a4198d8946ad204816516b82ea5e536f4b5e
Vick Aita <vickaita@gmail.com>
2014-03-22 02:51:47 -0400

    Prevent scale from dropping below 1.

--- src/cljs/limn/models/easel.cljs
+++ src/cljs/limn/models/easel.cljs
@@ -15,7 +15,9 @@
    :strokes []
    :width 0
    :height 0
-   :zoom 1
+   :scale 1
+   :offset-x 0
+   :offset-y 0
    :view-box [0 0 VBX VBX]})
 
 (defn start-stroke
@@ -36,15 +38,78 @@
         (assoc :strokes (conj (:strokes easel) (:current-stroke easel))))
     easel))
 
-(defn- left-most
-  [points]
-  (first (first (sort-by first points))))
-
-(defn- top-most
-  [points]
-  (second (first (sort-by second points))))
+;(defn- left-most
+;  [points]
+;  (first (first (sort-by first points))))
+;
+;(defn- top-most
+;  [points]
+;  (second (first (sort-by second points))))
+;
+;(defn- left-edge   [[[x1 _] [x2 _]]] (min x1 x2))
+;(defn- right-edge  [[[x1 _] [x2 _]]] (max x1 x2))
+;(defn- top-edge    [[[_ y1] [_ y2]]] (min y1 y2))
+;(defn- bottom-edge [[[_ y1] [_ y2]]] (max y1 y2))
 
 (defn zoom
   [easel from to]
-  (let [zoom (/ (apply distance from) (apply distance to))]
-    (conj easel {:view-box [0 0 (* zoom VBX) (* zoom VBX)]})))
+  (let [scale (/ (apply distance to) (apply distance from))
+        start (apply median-point from)
+        end (apply median-point to)
+        offset-x (- (first end) (first start))
+        offset-y (- (second end) (second start))]
+    (conj easel {:scale (max scale 1)
+                 :offset-x offset-x
+                 :offset-y offset-y})))
+
+;(defn zoom
+;  [easel zoom-box from to]
+;  (let [[[xa1 ya1] [xa2 ya2]] from
+;        [[xb1 yb1] [xb2 yb2]] to
+;        delta-x-a (- xa2 xa1)
+;        delta-x-b (- xb2 xb1)
+;        mx (/ delta-x-b delta-x-a)
+;        bx (- xb1 (* mx xa1))
+;        delta-y-a (- ya2 ya1)
+;        delta-y-b (- yb2 yb1)
+;        my (/ delta-y-b delta-y-a)
+;        by (- yb1 (* my ya1))
+;        [a b c d] zoom-box]
+;    (conj easel {:view-box [(+ (* mx a) bx)
+;                            (+ (* my b) by)
+;                            (+ (* mx c) bx)
+;                            (+ (* my d) by)]})))
+
+;;; Shift down
+;(= [0 -100 512 412]
+;   (:view-box (zoom default-easel
+;                    [0 0 512 512]
+;                    [[0 0] [100 0]]
+;                    [[0 100] [100 100]])))
+;
+;;; Shift right
+;(= [-100 0 412 512]
+;   (:view-box (zoom default-easel
+;                    [0 0 512 512]
+;                    [[0 0] [0 100]]
+;                    [[100 0] [100 100]])))
+;
+;;; Shift down and right
+;(= [-100 -100 412 412]
+;   (:view-box (zoom default-easel
+;                    [0 0 512 512]
+;                    [[0 0] [100 100]]
+;                    [[100 100] [200 200]])))
+;
+;;; Scale x
+;(= [0 0 256 256]
+;   (:view-box (zoom default-easel
+;                    [0 0 512 512]
+;                    [[0 0] [256 0]]
+;                    [[0 0] [512 0]])))
+;
+;(= [-256 -256 512 512]
+;   (:view-box (zoom default-easel
+;                    [0 0 512 512]
+;                    [[256 0] [512 0]]
+;                    [[0 0] [512 0]])))
