165fec6a258bb153959454eec3aedfc50af48630
Vick Aita <vickaita@gmail.com>
2014-06-15 07:48:34 -0400

    Defer reading credentials until running main.

--- resources/credentials/aws.example.clj
+++ resources/credentials/aws.example.clj
@@ -1,2 +1,3 @@
 {:aws-access-key "ACCESS_KEY"
- :aws-secret-key "SECRET_KEY"}
+ :aws-secret-key "SECRET_KEY"
+ :bucket-name "BUCKET_NAME"}
--- src/clj/limn/handler.clj
+++ src/clj/limn/handler.clj
@@ -19,37 +19,39 @@
   (:import (org.apache.commons.codec.binary Base64)
            (javax.imageio ImageIO)))
 
-(def bucket-name "spaghettipizza.us")
-
-(let [c (edn/read-string (slurp (io/resource "credentials/aws.clj")))]
-  (def creds {:access-key (:aws-access-key c)
-              :secret-key (:aws-secret-key c)}))
+(def ^:private creds (atom nil))
+(defn- aws-keys [] (select-keys @creds [:access-key :secret-key]))
+(defn- aws-bucket [] (:bucket-name @creds))
 
 (defn- ensure-bucket
+  "Checks to ensure that `bucket-name` exits on S3. If absent then it is
+  created. TODO: Gracefully handle exceptions here if the bucket cannot be
+  created. Also verify write permissions to the bucket."
   []
-  (println "Checking for the existence of the" bucket-name "bucket.")
-  (when-not (s3/bucket-exists? creds bucket-name)
-    (println "Creating the" bucket-name "bucket.")
-    (s3/create-bucket creds bucket-name)))
+  (println "Checking for the existence of the" (aws-bucket) "bucket.")
+  (when-not (s3/bucket-exists? (aws-keys) (aws-bucket))
+    (println "Creating the" (aws-bucket) "bucket.")
+    (s3/create-bucket (aws-keys) (aws-bucket))))
 
 (defn- upload-file
+  "Upload `file` to `bucket-name`."
   [file]
   (with-open [file-stream (io/input-stream file)]
     (let [file-hash (digest/md5 file)
           file-name (str "pizza/" file-hash ".png")
-          client (sdb/create-client (:access-key creds) (:secret-key creds))
+          client (sdb/create-client (:access-key (aws-keys)) (:secret-key (aws-keys)))
           config (assoc enc/keyword-strings :client client)]
-      (s3/put-object creds bucket-name file-name file-stream
+      (s3/put-object (aws-keys) (aws-bucket) file-name file-stream
                      {:content-length (.length file) :content-type "image/png"}
                      (s3/grant :all-users :read))
-      (sdb/put-attrs config bucket-name
+      (sdb/put-attrs config (aws-bucket)
                      {::sdb/id file-hash :created-at (System/currentTimeMillis)})
       [file-hash file-name])))
 
 (defn handle-file
   [file]
   (if file
-    (if-let [[file-hash file-name] (upload-file file)]
+    (if-let [[file-hash file-name] (upload-file (aws-bucket) (aws-keys) file)]
       {:status 200
        :headers {"Access-Control-Allow-Origin" "http://spaghettipizza.us"}
        :body (str {:file-name file-name :file-hash file-hash})}
@@ -59,10 +61,10 @@
      :body "Missing File."}))
 
 (defn pizza-list
-  []
-  (let [client (sdb/create-client (:access-key creds) (:secret-key creds))
+  [creds]
+  (let [client (sdb/create-client (:access-key (aws-keys)) (:secret-key (aws-keys)))
         config (assoc enc/keyword-strings :client client)
-        pizzas (sdb/query-all config (str "select * from `" bucket-name "`"))]
+        pizzas (sdb/query-all config (str "select * from `" (aws-bucket) "`"))]
     (map #(str "<a href=\"http://spaghettipizza.us/?pizza=" (get % ::sdb/id)
                ".png\"><img src=\"http://spaghettipizza.us/pizza/" (get % ::sdb/id)
                              ".png\"></a>") pizzas)))
@@ -70,6 +72,7 @@
 (defroutes all-routes
   (GET "/" {{debug :debug} :params}
        (pages/home (and debug (= :dev (:environment env)))))
+  (GET "/workspace" [] (pages/workspace))
   (mp/wrap-multipart-params
     (POST "/pizza/" {{{file :tempfile} :data} :params} (handle-file file)))
   (GET "/pizza/" [] (pizza-list))
@@ -77,10 +80,17 @@
   (route/files "/" {:root "resources/public"})
   (route/not-found (pages/error-404)))
 
+(defn get-creds []
+  (let [c (edn/read-string (slurp (io/resource "credentials/aws.clj")))]
+    {:access-key (:aws-access-key c)
+     :secret-key (:aws-secret-key c)
+     :bucket-name (:bucket-name c)}))
+
 (defn -main
   [& args]
   (println "Starting server; VERSION =" (:pizza-version env))
+  (reset! creds (get-creds))
   (if (= :dev (:environment env))
     (run-jetty (reload/wrap-reload (site #'all-routes)) {:port 8080})
-    (do (ensure-bucket)
+    (do (ensure-bucket creds)
         (run-jetty (site #'all-routes) {:port 8080}))))
